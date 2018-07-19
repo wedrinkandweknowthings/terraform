e928fdbffbb491482d5c6ea904c2a6f2b9e1941a
Nick Johns <nick.johns@gmail.com>
2018-07-19 11:53:12 +0100

    Fixed output to return multiple route tables

--- vpc/public-private/io.tf
+++ vpc/public-private/io.tf
@@ -26,5 +26,5 @@ output "gateway-id" { value = "${ module.vpc.gateway-id }" }
 output "id" { value = "${ module.vpc.id }" }
 output "subnet-ids-public" { value = ["${ module.vpc.subnet-ids-public }"] }
 output "route-table-id-main" { value = "${ module.vpc.route-table-id-main }" }
-output "route-table-id-private" { value = "${ aws_route_table.private.id }" }
+output "route-table-id-private" { value = "${ aws_route_table.private.*.id }" }
 output "subnet-ids-private" { value = ["${ aws_subnet.private.*.id }"] }
